a2366c81b5f9862efa1ae76d31595c7d598804a4
Renan Rhoden <renanrhoden@gmail.com>
2017-07-07 15:21:05 -0300

    add font to textview via databinding

--- app/src/main/java/com/ilegra/databinding/MyHandlers.java
+++ app/src/main/java/com/ilegra/databinding/MyHandlers.java
@@ -1,11 +1,7 @@
 package com.ilegra.databinding;
 
 import android.databinding.BaseObservable;
-import android.databinding.BindingAdapter;
 import android.view.View;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 public class MyHandlers extends BaseObservable{
 
--- app/src/main/java/com/ilegra/databinding/User.java
+++ app/src/main/java/com/ilegra/databinding/User.java
@@ -3,7 +3,11 @@ package com.ilegra.databinding;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.databinding.ObservableField;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -11,6 +15,14 @@ public class User extends BaseObservable {
     private final String firstName;
     private final String lastName;
     private boolean visible;
+    public ObservableField<Drawable> androidImage;
+    private int image = 0;
+    private String[] urls = {
+            "http://woikr.com/wp/wp-content/uploads/2011/02/chirag_androidified.png",
+            "https://www1-lw.xda-cdn.com/files/2011/02/11.jpg",
+            "http://img.talkandroid.com/uploads/2015/02/androidify_nba_update_portland_trail_blazers.png",
+            "https://jonuy.files.wordpress.com/2011/02/androidify.png?w=300&h=300",
+            "http://2.bp.blogspot.com/-DEH3eHSjMkE/TVlKIySmSlI/AAAAAAAAQWE/EDcAwLqC-So/s1600/android.png"};
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
@@ -32,8 +44,8 @@ public class User extends BaseObservable {
 
     @Bindable
     public void setVisible(boolean visible) {
-        notifyPropertyChanged(BR.visible);
         this.visible = visible;
+        notifyPropertyChanged(BR.visible);
     }
     @BindingAdapter("imageUrl")
     public static void loadImage(ImageView view, String imageUrl) {
@@ -41,13 +53,17 @@ public class User extends BaseObservable {
                 .load(imageUrl)
                 .into(view);
     }
+    @BindingAdapter({"bind:font"})
+    public static void setFont(TextView textView, String fontName){
+        textView.setTypeface(Typeface.createFromAsset(textView.getContext().getAssets(), "fonts/" + fontName));
+    }
 
     public String getImageUrl() {
-        return "https://www.android.com/static/2016/img/share/andy-lg.png";
+        if(image < urls.length){
+            image++;
+        } else {
+            image = 0;
+        }
+        return urls[image];
     }
-
-    public void setImageUrl(String imageUrl) {
-        //this.imageUrl = imageUrl;
-    }
-
 }
